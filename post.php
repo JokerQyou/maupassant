e8f8a2c2fa2d2d1a70278a9555e3eeb259d5f1ab
Joker Qyou <joker.qyou@gmail.com>
2015-10-10 22:28:58 +0800

    Tags and category in post page (with CSS icons)

--- post.php
+++ post.php
@@ -5,11 +5,27 @@
 			<header>
 				<h1 class="post-title"><?php $this->title() ?></h1>
 			</header>
-			<date class="post-meta">
+			<date class="post-meta meta-date">
 				<?php $this->date('F j, Y'); ?>
 			</date>
+			<div class="post-meta meta-category">
+				归类于 <?php $this->category(' '); ?>
+			</div>
 			<div class="post-content">
 				<?php $this->content(); ?>
+			</div>
+			<div class="post-meta meta-tags">
+				<?php if ($this->tags): ?>
+				<ul class="clearfix">
+				<?php foreach ($this->tags as $tag){
+					echo '<li><a href="'.$tag['permalink'].'">'.$tag['name'].'</a></li>';
+				}
+				?>
+				</ul>
+				<?php else: ?>
+				<!-- No tags -->
+				没有标签
+				<?php endif; ?>
 			</div>
 		</article>
 		<?php $this->need('comments.php'); ?>
